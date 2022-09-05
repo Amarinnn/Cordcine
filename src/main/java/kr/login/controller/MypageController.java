3bc7cd3e9a22cb3ebd648040e50efc21c1579f7b
pipi1055 <pipi1055@naver.com>
2022-09-05 17:11:18 +0900

    first

---
+++ src/main/java/kr/login/controller/MypageController.java
@@ -0,0 +1,30 @@
+package kr.login.controller;
+
+import java.io.IOException;
+
+import javax.servlet.ServletException;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import javax.servlet.http.HttpSession;
+
+import kr.login.dao.MemberMyBatisDAO;
+import kr.login.entity.Member;
+
+public class MypageController implements Controller {
+
+	@Override
+	public String requestProcessor(HttpServletRequest request, HttpServletResponse response)
+			throws ServletException, IOException {
+		
+		HttpSession session = request.getSession();
+		Member vo = (Member)session.getAttribute("mvo");
+		MemberMyBatisDAO dao= new MemberMyBatisDAO();
+		int cmw =dao.myWriteCount(vo.getLogin_id());
+	    session.setAttribute("cmw", cmw);
+	    int cmc =dao.myWriteCount(vo.getLogin_id());
+	    session.setAttribute("cmc", cmc);
+		
+		return "mypage/mypage";
+	}
+
+}
